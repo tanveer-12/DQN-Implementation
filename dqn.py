9f1d9aa2bab9a615d9cd7b2909462636188c1e73
Tanveer Kaur <tanveerkaur1292@outlook.com>
2025-11-03 22:08:46 -0500

    Dqn algo

--- dqn.py
+++ dqn.py
@@ -22,11 +22,76 @@ class Network(nn.Module):
         in_features = int(np.prod(env.observation_space.shape))
 
         self.net = nn.Sequential(
-            
+            nn.Linear(in_features, 65),
+            nn.Tanh(),
+            nn.Linear(64, env.action_space.n)
         )
+
+    def forward(self, x):
+        return self.net(x)
+
+    def act(self, obs):
+        obs_t = torch.as_tensor(obs, dtype=torch.float32)
+        q_values = self(obs_t.unsqueeze(0))
+
+        max_q_index = torch.argmax(q_values, dim=1)[0]
+        action = max_q_index.detach().item()
+        return action
 
 env = gym.make('CartPole-v0')
 replay_buffer = deque(maxlen=BUFFER_SIZE)
 rew_buffer = deque([0.0], maxlen=100)
 
 episode_reward = 0.0
+
+online_net = Network(env)
+target_net = Network(env)
+
+target_net.load_state_dict(online_net.state_dict())
+
+# initialize Replay Buffer
+obs = env.reset()
+for _ in range(MIN_REPLAY_SIZE):
+    action = env.action_space.sample()
+
+    new_obs, rew, done, _ = env.step(action )
+    transition = (obs, action, rew, new_obs)
+    replay_buffer.append(transition)
+    obs = new_obs
+
+    if done:
+        obs = env.reset()
+
+
+# Main Training Loop
+obs = env.reset()
+for step in itertools.count():
+    epsilon = np.interp(step, [0, EPSILON_DECAY],[EPSILON_START, EPSILON_END])
+
+    rnd_sample = random.random()
+    if rnd_sample <= epsilon:
+        action = env.action_space.sample()
+    else:
+        action = online_net.act(obs)
+
+    new_obs, rew, done, _ = env.step(action )
+    transition = (obs, action, rew, new_obs)
+    replay_buffer.append(transition)
+    obs = new_obs
+
+    episode_reward += rew
+
+    if done:
+        obs = env.reset()
+
+        rew_buffer.append(episode_reward)
+        episode_reward = 0.0
+
+
+    # Start Gradient Step
+    transitions = random.sample(replay_buffer, BATCH_SIZE)
+    obses = np.asarray([t[0] for t in transitions])
+    actions = np.asarray([t[1] for t in transitions])
+    rews = np.asarray([t[2] for t in transitions])
+    dones = np.asarray([t[3] for t in transitions])
+    new_obses = np.asarray([t[4] for t in transitions])
